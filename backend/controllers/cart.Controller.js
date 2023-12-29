a53f7c6f09daaa05b70d4c1c9665f680883465eb
AYUB SHAH <ayubshah810@gmail.com>
2023-12-29 21:34:22 +0500

    add to cart added

--- backend/controllers/cart.Controller.js
+++ backend/controllers/cart.Controller.js
@@ -1,49 +1,98 @@
 const Cart = require("../models/cart.Model.js");
-const User = require("../models/user.model.js");
 
-const cartController = {
-    addToCart: async (req, res) => {
-      try {
-        const { productId, quantity } = req.body;
-        const userId = req.user.id; // Assuming you have user authentication middleware
-  
-        // Check if the product exists
-        const product = await Product.findById(productId);
-        if (!product) {
-          return res.status(404).json({ error: 'Product not found' });
-        }
-  
-        // Check if the user exists
-        const user = await User.findById(userId);
-        if (!user) {
-          return res.status(404).json({ error: 'User not found' });
-        }
-  
-        // Check if the product is already in the user's cart
-        const userCart = await Cart.findOne({ userId });
-        const existingProductIndex = userCart.products.findIndex(
-          (item) => item.productId.toString() === productId
-        );
-  
-        if (existingProductIndex !== -1) {
-          // If the product already exists in the cart, update its quantity
-          userCart.products[existingProductIndex].quantity += parseInt(quantity);
-        } else {
-          // If the product doesn't exist in the cart, add it
-          userCart.products.push({ productId, quantity: parseInt(quantity) });
-        }
-  
-        // Save the updated cart
-        await userCart.save();
-  
-        res.status(200).json({ message: 'Product added to cart successfully' });
-      } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal server error' });
+
+
+// Controller function to add a product to the cart
+exports.addToCart = async (req, res) => {
+  const { userId, productId, quantity } = req.body;
+
+  try {
+    let cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      cart = new Cart({ userId, items: [{ productId, quantity }] });
+      await cart.save();
+    } else {
+      const itemExists = cart.items.find(item => item.productId.equals(productId));
+
+      if (itemExists) {
+        itemExists.quantity += quantity;
+      } else {
+        cart.items.push({ productId, quantity });
       }
-    },
-  
-    // Other cart functions like removeFromCart, updateCart, viewCart can be implemented similarly
-  };
-  
-  module.exports = cartController;
+
+      await cart.save();
+    }
+
+    res.status(200).json({ message: 'Product added to cart successfully' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Failed to add product to cart' });
+  }
+};
+
+// Get cart items 
+exports.getCartItems = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const cart = await Cart.findOne({ userId }).populate('items.productId');
+
+    if (!cart) {
+      return res.status(404).json({ message: 'Cart not found' });
+    }
+
+    res.status(200).json(cart);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to get cart items' });
+  }
+};
+
+// Update cart items
+exports.updateCartItemQuantity = async (req, res) => {
+  const { userId, productId, quantity } = req.body;
+
+  try {
+    const cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      return res.status(404).json({ message: 'Cart not found' });
+    }
+
+    const cartItem = cart.items.find(item => item.productId === productId);
+
+    if (!cartItem) {
+      return res.status(404).json({ message: 'Item not found in the cart' });
+    }
+
+    cartItem.quantity = quantity;
+    await cart.save();
+
+    res.status(200).json({ message: 'Cart item quantity updated successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to update cart item quantity' });
+  }
+};
+// Remove cart item 
+
+exports.removeItemFromCart = async (req, res) => {
+  const { userId, productId } = req.params;
+
+  try {
+    const cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      return res.status(404).json({ message: 'Cart not found' });
+    }
+
+    cart.items = cart.items.filter(item => item.productId !== productId);
+    await cart.save();
+
+    res.status(200).json({ message: 'Item removed from cart successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to remove item from cart' });
+  }
+};
--- backend/routes/index.route.js
+++ backend/routes/index.route.js
@@ -5,15 +5,17 @@ const userRoute = require('./user.route');
 const emailRoute = require('./email.route');
 const imageRoute = require('./image.route');
 const productRoute = require('./product.route');
+const cartRoute = require("../routes/cart.Route.js");
 
 
 router.use("/contact", contactRoute);
 router.use("/user", userRoute);
-router.use("/email", Email);
+router.use("/email", emailRoute);
 router.use('/uploads', imageRoute);
 
 // Ensure that the '/products/:id' route is defined after '/products'
 router.use('/', productRoute);
+router.use('/', cartRoute);
 
 
 
